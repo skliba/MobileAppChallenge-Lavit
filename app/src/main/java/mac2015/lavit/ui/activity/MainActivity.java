a2532b6e7a8f53d01ae4a6a08c344ecd96f9a930
David Macan <dmacan23@gmail.com>
2015-09-23 13:52:29 +0200

    Created MainActivity Added Smartadapter, LoadingView, RecyclerView and CardView dependencies Created activity_main layout

---
+++ app/src/main/java/mac2015/lavit/ui/activity/MainActivity.java
@@ -0,0 +1,22 @@
+package mac2015.lavit.ui.activity;
+
+import android.os.Bundle;
+
+import mac2015.lavit.R;
+import mac2015.lavit.app.BaseActivity;
+
+/**
+ * Created by dmacan on 23.9.2015..
+ */
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected int provideLayoutResource() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void main(Bundle savedInstanceState) {
+
+    }
+}
