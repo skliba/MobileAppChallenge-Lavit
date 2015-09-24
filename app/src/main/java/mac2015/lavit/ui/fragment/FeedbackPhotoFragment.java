88d5abab66f63a33a8a0c1722ac6488067abdaca
David Macan <dmacan23@gmail.com>
2015-09-24 13:24:59 +0200

    Feedback photo update
    Updated fragment for photo feedback

--- app/src/main/java/mac2015/lavit/ui/fragment/FeedbackPhotoFragment.java
+++ app/src/main/java/mac2015/lavit/ui/fragment/FeedbackPhotoFragment.java
@@ -1,13 +1,27 @@
 package mac2015.lavit.ui.fragment;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import butterknife.InjectView;
+import butterknife.OnClick;
 import mac2015.lavit.R;
 
 /**
@@ -15,6 +29,11 @@ import mac2015.lavit.R;
  */
 public class FeedbackPhotoFragment extends FeedbackFragment<File> {
 
+    private static final String TAG = "DAM_FRAG_PHOTO";
+    @InjectView(R.id.imgFeedbackPhoto)
+    ImageView imgFeedbackPhoto;
+    private String currentPhotoPath;
+    private File photoFile;
 
     @Nullable
     @Override
@@ -25,5 +44,61 @@ public class FeedbackPhotoFragment extends FeedbackFragment<File> {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    @OnClick(R.id.fab)
+    protected void onFabClick() {
+        takePicture();
+    }
+
+    private void takePicture() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create the File where the photo should go
+        photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException ex) {
+            Log.e(TAG, "IOE", ex);
+            // Error occurred while creating the File
+        }
+        // Continue only if the File was successfully created
+        if (photoFile != null) {
+            Log.i(TAG, "PF!=null");
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                    Uri.fromFile(photoFile));
+            startActivityForResult(takePictureIntent, 0);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.i(TAG, "AFR:" + requestCode);
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK) {
+            Picasso.with(getActivity()).load(photoFile).resize(500, 500).into(imgFeedbackPhoto);
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+
+    @Override
+    public File getFeedback() {
+        return photoFile;
     }
 }
